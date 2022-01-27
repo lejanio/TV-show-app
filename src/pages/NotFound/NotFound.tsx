f8d814f9c7dc6461cb4aa01208a51c694c390c8b
lejanio <janis.leonciks@gmail.com>
2022-01-27 11:49:56 +0200

    Back added to NotFound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './NotFound.scss';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-  <div className="error-message--container">
-    <span className="error-message">404 - Page not found</span>
+  <div className="show--container">
+    <nav>
+      <Link to="/" className="link">&lt; Back to search</Link>
+    </nav>
+    <div className="error-message--container">
+      <span className="error-message">404 - Page not found</span>
+    </div>
   </div>
 );
 
